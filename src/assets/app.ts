c73e1573c8e2ffd64ff9bfa11f33270e8081205f
Callum <callumbonnyman@me.com>
2021-07-05 22:18:43 +0400

    Added more subtle styling and emoji favicon.

--- src/assets/app.ts
+++ src/assets/app.ts
@@ -2,62 +2,49 @@ import { LitElement, html, customElement, property, css } from 'lit-element';
 
 @customElement('pizza-counter')
 class PizzaCounter extends LitElement {
-	@property()
-	count = 0;
+  @property()
+  count = 0;
 
-	connectedCallback() {
-		super.connectedCallback()
-		setInterval(() => {
-			++this.count;
-			if (this.count % 20 === 0) {
-				plausible('Pizza views', Math.floor(this.count / 20));
-			}
-		}, 3000);
-	}
+  connectedCallback() {
+    super.connectedCallback();
+    setInterval(() => {
+      ++this.count;
+      if (this.count % 20 === 0) {
+        plausible('Pizza views', Math.floor(this.count / 20));
+      }
+    }, 3000);
+  }
 
-	render() {
-		return html`<div>
-			<h1>Pizza Watched</h1>
-			<p>${this.count}</p>
-		</div>`;
-	}
+  render() {
+    return html`<p class="${this.count > 0 ? 'show' : ''}">
+      🍕 Count <span>${this.count}</span>
+    </p>`;
+  }
 
-	static get styles() {
-		return css`
-      div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+  static get styles() {
+    return css`
+      p {
         color: white;
         text-align: center;
-        padding: 1rem;
-			}
-			
-			h1 {
-        display: inline-block;
-        padding: 0.2em 0.6em;
-        border-radius: 0.4rem;
-				margin-bottom: 0.2em;
-        color: white;
-        font-size: clamp(1.333rem, 5vw, 2em);
-        font-weight: bold;
-        text-transform: uppercase;
-        letter-spacing: 0.2em;
-        background-color: var(--primary);
-        line-height: 1.2;
-			}
-			
-			p {
-        padding: 0.1em 0.3em;
-        border-radius: 0.4rem;
+        padding: 0.33em 1em;
         margin: 0;
-        line-height: 1;
-        font-size: clamp(2rem, 10vw, 4rem);
+        background-color: rgba(255, 255, 255, 0.5);
+        backdrop-filter: blur(8px);
+        color: rgb(var(--primary));
+        font-size: clamp(1.2rem, 5vw, 1.6rem);
         font-weight: bold;
-        color: white;
-        font-family: monospace;
-				background-color: #E85D04;
-			}
+        line-height: 1.2;
+        transition: transform 100ms ease-out;
+        transform: translateY(-100%);
+      }
+
+      p.show {
+        transform: translateY(0%);
+      }
+
+      span {
+        margin-left: 0.1em;
+      }
     `;
-	}
+  }
 }
