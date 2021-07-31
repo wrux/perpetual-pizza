e945eb10cc9649254b83eae4b2ad2ba93ae7309b
Callum <callumbonnyman@me.com>
2021-07-31 19:55:37 +0400

    Added Cloudflare video provider for more performant video loading.

--- src/_assets/app.js
+++ src/_assets/app.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
+import Hls from 'hls.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const video = document.getElementById('cover-video');
+  const videoId = video?.dataset?.videoId;
+
+  if (videoId) {
+    let hlsVideo = new Hls();
+    hlsVideo.loadSource(
+      `https://videodelivery.net/${videoId}/manifest/video.m3u8`
+    );
+    hlsVideo.attachMedia(video);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
   const tray = document.querySelector('[data-tray]');
   if (!tray) {
     return;
