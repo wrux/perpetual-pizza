be3c2bc8f9dff6f40e29ca80e0522ae90a91974d
Callum Bonnyman <callumbonnyman@me.com>
2023-07-06 21:18:11 +0700

    Refactored website to use AstroJS. (#48)

---
+++ src/data/pizzas.ts
@@ -0,0 +1,11 @@
+export const pizzas: Array<{
+  title: string;
+  slug: string;
+}> = [
+  { title: 'PPZ01', slug: '01' },
+  { title: 'PPZ02', slug: '02' },
+  { title: 'PPZ03', slug: '03' },
+  { title: 'PPZ04', slug: '04' },
+  { title: 'PPZ05', slug: '05' },
+  { title: 'PPZ06', slug: '06' },
+];
